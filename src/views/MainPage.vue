2764b23185fdf2ee293b338452f4ce6d99d826ee
Likit Preeyanon <likit.pre@mahidol.edu>
2021-02-04 08:55:04 +0700

    added buefy, fontawesome
    added main page in views

---
+++ src/views/MainPage.vue
@@ -0,0 +1,42 @@
+<template>
+<section class="section">
+  <div class="columns">
+    <div class="column has-text-centered is-one-third is-offset-4">
+      <h1 class="title">NCD Sarcopenia Survey</h1>
+      <nav class="panel">
+        <p class="panel-heading">
+          Menu
+        </p>
+        <a class="panel-block">
+          <span class="panel-icon">
+            <i class="fas fa-book" aria-hidden="true"></i>
+          </span>
+          Register
+        </a>
+        <a class="panel-block">
+          <span class="panel-icon">
+            <i class="fas fa-book" aria-hidden="true"></i>
+          </span>
+          Records
+        </a>
+        <a class="panel-block">
+          <span class="panel-icon">
+            <i class="fas fa-book" aria-hidden="true"></i>
+          </span>
+          New Record
+        </a>
+      </nav>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+name: "MainPage"
+}
+</script>
+
+<style scoped>
+
+</style>
