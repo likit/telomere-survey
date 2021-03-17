7cf139f39e2e6ec06b5f5dbe898ff1a62a469732
Likit Preeyanon <likit.pre@mahidol.edu>
2021-03-17 16:18:32 +0700

    added a link to the profile page

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -3,17 +3,17 @@
   <div class="columns">
     <div class="column has-text-centered is-one-third is-offset-4">
       <h1 class="title">NCD Sarcopenia Survey</h1>
-      <h1 class="subtitle">Welcome {{ user.email }}</h1>
+      <h1 class="subtitle">Welcome {{ user.displayName }}</h1>
       <nav class="panel">
         <p class="panel-heading">
           Menu
         </p>
-        <a class="panel-block">
+        <router-link class="panel-block" :to="{name: 'Profile'}">
           <span class="panel-icon">
             <i class="fas fa-user-alt" aria-hidden="true"></i>
           </span>
           Profile
-        </a>
+        </router-link>
         <a class="panel-block">
           <span class="panel-icon">
             <i class="fas fa-book" aria-hidden="true"></i>
