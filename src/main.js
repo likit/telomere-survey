ab2fe24e2694405b4644e7e6366d6d23af614073
Likit Preeyanon <likit.pre@mahidol.edu>
2021-03-24 08:56:46 +0700

    added state for SARCF view

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import Buefy from "buefy"
 import "buefy/dist/buefy.css"
 import router from './router'
+import store from './store'
 import { auth } from './firebase'
 
 Vue.config.productionTip = false
@@ -14,5 +15,6 @@ auth.onAuthStateChanged(user => {
 
 new Vue({
     router,
+    store,
     render: h => h(App),
 }).$mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -2,3 +2,23 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+
+function initializeForm() {
+    return {
+        record: {
+            sarcf: {
+                one: null,
+                two: null,
+                three: null,
+                four: null,
+                five: null,
+            }
+        }
+    }
+}
+
+export default new Vuex.Store({
+    state: {
+        form: initializeForm(),
+    }
+})
