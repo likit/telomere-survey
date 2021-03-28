05f087fd0092dc7f9056e687940c4a2f9f83bcb7
Likit Preeyanon <likit.pre@mahidol.edu>
2021-03-28 12:02:09 +0700

    fixed issue when updating selected choices

--- src/views/MiniCog.vue
+++ src/views/MiniCog.vue
@@ -18,25 +18,33 @@
           </ul>
         </b-field>
         <b-field label="ให้ตั้งใจฟังดี ๆ เดี๋ยวจะบอกคำ 3 คำ เมื่อพูดจบแล้วให้พูดตามและจำไว้เดี๋ยวจะกลับมาถามซ้ำ">
-          <b-checkbox @input="updateScore($event, 'หลานสาว')">หลานสาว</b-checkbox>
+          <b-checkbox @input="updateScore('หลานสาว')" v-model="q1">หลานสาว</b-checkbox>
         </b-field>
         <b-field>
-          <b-checkbox @input="updateScore($event, 'สวรรค์')">สวรรค์</b-checkbox>
+          <b-checkbox @input="updateScore('สวรรค์')" v-model="q2">สวรรค์</b-checkbox>
         </b-field>
         <b-field>
-          <b-checkbox @input="updateScore($event, 'ภูเขา')">ภูเขา</b-checkbox>
+          <b-checkbox @input="updateScore('ภูเขา')" v-model="q3">ภูเขา</b-checkbox>
         </b-field>
       </b-step-item>
     </b-steps>
     <div class="buttons is-centered">
-      <button class="button is-light" @click="$router.back()">Back</button>
+      <button class="button is-light" @click="$router.back()">
+        <span class="icon">
+          <i class="fas fa-chevron-left"></i>
+        </span>
+      </button>
       <button class="button is-primary" @click="saveData">
         <span class="icon">
           <i class="far fa-save"></i>
         </span>
         <span>Save</span>
       </button>
-      <router-link :to="{ name: 'Clock' }" class="button is-success">Next</router-link>
+      <router-link :to="{ name: 'Clock' }" class="button is-success">
+        <span class="icon">
+          <i class="fas fa-chevron-right"></i>
+        </span>
+      </router-link>
     </div>
   </div>
 </section>
@@ -49,11 +57,35 @@ import {mapState} from "vuex";
 export default {
   name: "MiniCog",
   components: {Navigation},
+  data() {
+    return {
+      q1: null,
+      q2: null,
+      q3: null,
+    }
+  },
   computed: {
     ...mapState(['form'])
   },
+  mounted() {
+    let self = this
+    console.log(this.form.record.minicog.items, 'mounted')
+    this.form.record.minicog.items.forEach((d)=>{
+      switch (d) {
+        case "หลานสาว":
+          self.q1 = true;
+          break;
+        case "สวรรค์":
+          self.q2 = true;
+          break;
+        case "ภูเขา":
+          self.q3 = true;
+          break;
+      }
+    })
+  },
   methods: {
-    updateScore: function (event, item) {
+    updateScore: function (item) {
       this.$store.commit('updateMiniCogItems', item)
     },
     saveData() {
@@ -71,26 +103,14 @@ export default {
         })
       } else {
         this.$store.dispatch('saveForm').then(() => {
-          self.$buefy.dialog.alert({
-            title: 'Login Successful',
+          self.$buefy.toast.open({
             message: 'บันทึกข้อมูลเรียบร้อยแล้ว',
             type: 'is-success',
-            hasIcon: true,
-            icon: 'check-circle',
-            iconPack: 'fa',
-            ariaRole: 'alertdialog',
-            ariaModal: true,
           })
         }).catch(() => {
-          self.$buefy.dialog.alert({
-            title: 'Error!',
+          self.$buefy.toast.open({
             message: 'โปรแกรมไม่สามารถบันทึกข้อมูลได้ โปรดลองใหม่อีกครั้ง',
             type: 'is-danger',
-            hasIcon: true,
-            icon: 'times-circle',
-            iconPack: 'fa',
-            ariaRole: 'alertdialog',
-            ariaModal: true
           })
         })
       }
